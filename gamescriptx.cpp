4eae3b7db143c95ae6fe76e789f107633c84ba0a
Constantine Tarasenkov <iamvfx@gmail.com>
2022-03-07 15:30:35 +0300

    Initial sketch

---
+++ gamescriptx.cpp
@@ -0,0 +1,28 @@
+typedef union Generic64BitValue {
+  int64_t  i;
+  uint64_t u;
+  double   d;
+  void *   p;
+} Generic64BitValue;
+
+extern std::map<string, std::map<string, std::map<string, Generic64BitValue>>> globalEntryGroupKey;
+
+#define gsxCreateHandle(HANDLE_TYPE, HANDLE_NAME, CREATE_HANDLE_PROCEDURE_CALL) \
+  if (globalEntryGroupKey["handle"][HANDLE_TYPE][HANDLE_NAME].p == 0) { \
+    globalEntryGroupKey["handle"][HANDLE_TYPE][HANDLE_NAME].p = (void *)CREATE_HANDLE_PROCEDURE_CALL; \
+  }
+
+int frame(int recompileRequested, void * dataFromMain) {
+  int recompile = recompileRequested;
+  
+  // User code here
+  
+  if (recompile == 1) {
+    for (auto & pair : globalEntryGroupKey["handle"]["SomeHandleType"]) {
+      void * handle = pair.second.p;
+      // Destroy the handle here
+    }
+    // More for loops as above for other handle types here
+  }
+  return recompile;
+}
