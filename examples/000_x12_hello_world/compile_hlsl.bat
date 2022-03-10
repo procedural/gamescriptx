97ce027b254fa2abcbd3bbcabc5a2e8c6f9e6c71
Constantine Tarasenkov <iamvfx@gmail.com>
2022-03-11 00:34:13 +0300

    Compile HLSL code with debug info

--- examples/000_x12_hello_world/compile_hlsl.bat
+++ examples/000_x12_hello_world/compile_hlsl.bat
@@ -1,2 +1,2 @@
-"C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/fxc.exe" /T vs_5_1 /E VSMain program.hlsl /Fh program.hlsl.vs.h
-"C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/fxc.exe" /T ps_5_1 /E PSMain program.hlsl /Fh program.hlsl.ps.h
+"C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/fxc.exe" /Zi /Od /T vs_5_1 /E VSMain program.hlsl /Fh program.hlsl.vs.h
+"C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/fxc.exe" /Zi /Od /T ps_5_1 /E PSMain program.hlsl /Fh program.hlsl.ps.h
