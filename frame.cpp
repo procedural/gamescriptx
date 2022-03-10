307d15de9f923d747e82627b3a36885c46264934
Constantine Tarasenkov <iamvfx@gmail.com>
2022-03-10 16:02:12 +0300

    Don't need this piece of example code

--- frame.cpp
+++ frame.cpp
@@ -5,12 +5,5 @@ extern "C" __declspec(dllexport) int frame(int recompileRequested, void * dataFr
   
   // User code here
   
-  if (recompile == 1) {
-    for (auto & pair : globalCache[""]["SomeHandleType"]) {
-      void * handle = pair.second.value.p;
-      // Destroy the handle here
-    }
-    // More for loops as above for other handle types here
-  }
   return recompile;
 }
