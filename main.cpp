e70979a1227a7f9cca83bd87c5a4598b0cbcf03f
Constantine Tarasenkov <iamvfx@gmail.com>
2022-03-10 15:57:52 +0300

    Destroy all X12 handles

--- main.cpp
+++ main.cpp
@@ -54,7 +54,89 @@ static void recompileDll(void * x12DebugContext) {
     // TODO(Constantine): Serialize the global storage to disk and allow to load it by dragging-and-dropping it onto a Game Script X window.
   }
   {
-    // TODO(Constantine): Destroy all X12 handles here by iterating over all cache entries and cache handle type groups, assuming the user has finished all the threads in the frame procedure on receiving a recompile request.
+    // NOTE(Constantine): Destroy all X12 handles here by iterating over all cache entries and cache handle type groups, assuming the user has finished all the threads in the frame procedure on receiving a recompile request.
+    for (auto & entryPair : globalCache) {
+      std::map<std::string, GenericElement> factories                  = entryPair.second["X12Factory4"];
+      std::map<std::string, GenericElement> adapters                   = entryPair.second["X12Adapter3"];
+      std::map<std::string, GenericElement> devices                    = entryPair.second["X12Device3"];
+      std::map<std::string, GenericElement> heaps                      = entryPair.second["X12Heap"];
+      std::map<std::string, GenericElement> resources                  = entryPair.second["X12Resource"];
+      std::map<std::string, GenericElement> descriptorHeaps            = entryPair.second["X12DescriptorHeap"];
+      std::map<std::string, GenericElement> blobs                      = entryPair.second["X12Blob"];
+      std::map<std::string, GenericElement> rootSignatures             = entryPair.second["X12RootSignature"];
+      std::map<std::string, GenericElement> rootSignatureDeserializers = entryPair.second["X12RootSignatureDeserializer"];
+      std::map<std::string, GenericElement> pipelineStates             = entryPair.second["X12PipelineState"];
+      std::map<std::string, GenericElement> fences                     = entryPair.second["X12Fence"];
+      std::map<std::string, GenericElement> commandQueues              = entryPair.second["X12CommandQueue"];
+      std::map<std::string, GenericElement> commandAllocators          = entryPair.second["X12CommandAllocator"];
+      std::map<std::string, GenericElement> commandLists               = entryPair.second["X12CommandList"];
+      std::map<std::string, GenericElement> swapChains                 = entryPair.second["X12SwapChain3"];
+      std::map<std::string, GenericElement> outputs                    = entryPair.second["X12Output4"];
+      for (auto & pair : fences) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : commandLists) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : commandAllocators) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : pipelineStates) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : rootSignatureDeserializers) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : rootSignatures) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : blobs) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : descriptorHeaps) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : resources) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : heaps) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : outputs) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : swapChains) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : commandQueues) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : devices) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : adapters) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+      for (auto & pair : factories) {
+        void * handle = pair.second.value.p;
+        x12Release(handle, __FILE__, __LINE__);
+      }
+    }
   }
   if (x12DebugContext != 0) {
     x12DebugReport(x12DebugContext, __FILE__, __LINE__);
