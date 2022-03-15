3418e70fe39418b7d6d924239ac5f0c26dabd544
Constantine Tarasenkov <iamvfx@gmail.com>
2022-03-15 16:11:39 +0300

    Rename default shader code global variables' names

--- examples/001_x12_drawing_meshes/compile_hlsl.bat
+++ examples/001_x12_drawing_meshes/compile_hlsl.bat
@@ -1,4 +1,8 @@
 "C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/fxc.exe" /Zi /Od /T vs_5_1 /E VSMain depthpass.hlsl /Fh depthpass.hlsl.vs.h
+powershell -Command "(gc depthpass.hlsl.vs.h) -replace 'g_VSMain', 'g_DepthPassVSMain' | Out-File -encoding ASCII depthpass.hlsl.vs.h"
 
 "C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/fxc.exe" /Zi /Od /T vs_5_1 /E VSMain scenepass.hlsl /Fh scenepass.hlsl.vs.h
+powershell -Command "(gc scenepass.hlsl.vs.h) -replace 'g_VSMain', 'g_ScenePassVSMain' | Out-File -encoding ASCII scenepass.hlsl.vs.h"
+
 "C:/Program Files (x86)/Windows Kits/10/bin/10.0.19041.0/x64/fxc.exe" /Zi /Od /T ps_5_1 /E PSMain scenepass.hlsl /Fh scenepass.hlsl.ps.h
+powershell -Command "(gc scenepass.hlsl.ps.h) -replace 'g_PSMain', 'g_ScenePassPSMain' | Out-File -encoding ASCII scenepass.hlsl.ps.h"
