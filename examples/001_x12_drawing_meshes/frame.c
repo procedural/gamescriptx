2c2d6a412345ffb07d1d1794c03bffc8934948ed
Constantine Tarasenkov <iamvfx@gmail.com>
2022-04-04 23:04:57 +0300

    Print matrix data as floats

--- examples/001_x12_drawing_meshes/frame.c
+++ examples/001_x12_drawing_meshes/frame.c
@@ -159,6 +159,28 @@ __declspec(dllexport) int frame(int recompileRequested, void * dataFromMain) {
       3247554907, 3234536323, 3250406506, 3240057887,
       1099796719, 1086670109, 1102566361, 1092616181,
     };
+    {
+      float * F = (float *)(void *)arrayConstantBufferCameraMatricesData;
+      printf(
+        "arrayConstantBufferCameraMatricesData:\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g\n",
+        F[0], F[1], F[2], F[3],
+        F[4], F[5], F[6], F[7],
+        F[8], F[9], F[10],F[11],
+        F[12],F[13],F[14],F[15],
+        F[16],F[17],F[18],F[19],
+        F[20],F[21],F[22],F[23],
+        F[24],F[25],F[26],F[27],
+        F[28],F[29],F[30],F[31]
+      );
+    }
     unsigned * p = (unsigned *)arrayConstantBufferCameraMatricesMemoryPointer;
     for (unsigned i = 0; i < _countof(arrayConstantBufferCameraMatricesData); i += 1) {
       p[i] = arrayConstantBufferCameraMatricesData[i];
@@ -204,6 +226,28 @@ __declspec(dllexport) int frame(int recompileRequested, void * dataFromMain) {
       1111921480, 3267983338, 3229376151, 3240057937,
       3259340525, 1120435481, 1081810399, 1092616233,
     };
+    {
+      float * F = (float *)(void *)arrayConstantBufferLightMatricesData;
+      printf(
+        "arrayConstantBufferLightMatricesData:\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g,\n"
+        "%.9g, %.9g, %.9g, %.9g\n",
+        F[0], F[1], F[2], F[3],
+        F[4], F[5], F[6], F[7],
+        F[8], F[9], F[10],F[11],
+        F[12],F[13],F[14],F[15],
+        F[16],F[17],F[18],F[19],
+        F[20],F[21],F[22],F[23],
+        F[24],F[25],F[26],F[27],
+        F[28],F[29],F[30],F[31]
+      );
+    }
     unsigned * p = (unsigned *)arrayConstantBufferLightMatricesMemoryPointer;
     for (unsigned i = 0; i < _countof(arrayConstantBufferLightMatricesData); i += 1) {
       p[i] = arrayConstantBufferLightMatricesData[i];
